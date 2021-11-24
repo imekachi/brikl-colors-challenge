e6af371164b147e2fd40941c4a8e36744a739cfe
imekachi <imekachi@gmail.com>
2021-11-25 00:41:36 +0700

    refactor(backend): make adding formatter easier
    - dynamically create the GraphQL color types enum from the `colorFormatterMap`

--- src/backend/graphql/typeDefs.ts
+++ src/backend/graphql/typeDefs.ts
@@ -1,9 +1,11 @@
 import { gql } from 'apollo-server-micro'
+import { colorFormatterMap } from '@/service/color/formatter'
+
+const colorTypeEnumValues = Object.keys(colorFormatterMap).join('\n')
 
 export const typeDefs = gql`
   enum ColorTypes {
-    RGB
-    HEX
+    ${colorTypeEnumValues}
   }
 
   type Color {
